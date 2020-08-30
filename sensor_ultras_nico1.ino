3f91770017750cc41b5ff41fb6914e4c418aa71d
Vanderlei Fontenelle <vanderleifonte@hotmail.com>
2020-08-30 13:25:45 -0300

    Add files via upload

---
+++ sensor_ultras_nico1.ino
@@ -0,0 +1,29 @@
+int inches = 0;
+
+int cm = 0;
+
+long readUltrasonicDistance(int triggerPin, int echoPin)
+{
+  pinMode(triggerPin, OUTPUT);  // Clear the trigger
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigger pin to HIGH state for 10 microseconds
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  pinMode(echoPin, INPUT);
+  // Reads the echo pin, and returns the sound wave travel time in microseconds
+  return pulseIn(echoPin, HIGH);
+}
+
+void setup()
+{
+  Serial.begin(9600);
+
+}
+
+void loop()
+{
+  Serial.println(0.01723 * readUltrasonicDistance(7, 7));
+  delay(10); // Delay a little bit to improve simulation performance
+}
